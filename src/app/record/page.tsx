fa530385ec4ed1fa4c85a2c98418688fd04d6496
yash <yashgouravkar@gmail.com>
2023-11-07 11:16:08 +0530

    commented everything /record

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,19 +1,19 @@
 'use client';
-import ReactVideoRecorder from 'react-video-recorder';
+import VideoRecorder from 'react-video-recorder';
 
 export default function Page() {
   return (
-    <div className="container mx-auto h-screen flex flex-col justify-center items-center">
-      <div className="self-center">
-        {/* <Webcam mirrored audio={false} /> */}
-      </div>
-      <ReactVideoRecorder
-        isOnInitially
-        onRecordingComplete={(videoBlob: any) => {
-          // Do something with the video...
-          console.log('videoBlob', videoBlob);
-        }}
-      />
-    </div>
+    // <div className="container mx-auto h-screen flex flex-col justify-center items-center">
+    //   <div className="self-center">
+    //     {/* <Webcam mirrored audio={false} /> */}
+    //   </div>
+    <VideoRecorder
+      isOnInitially
+      onRecordingComplete={(videoBlob: any) => {
+        // Do something with the video...
+        console.log('videoBlob', videoBlob);
+      }}
+    />
+    // </div>
   );
 }
